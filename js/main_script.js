985a84f5412d31d3e7c2fcb99b5b0e6f6ef7fb19
kevinvanoevelen <kevinvanoevelen@MacBook-Pro-van-kevin.local>
2015-12-26 05:16:06 +0100

    temp commit
    let’s see what we have so far

--- js/main_script.js
+++ js/main_script.js
@@ -1,9 +1,16 @@
 /* MAIN JAVASCRIPT PAGE */
 
-/* TITLE */
+/* adjusting the height of the #wrapper */
+/* setting min-height to window height minus the top and bottom margins */
 
+var inner_width = window.innerWidth;
+var inner_height = window.innerHeight;
 
+var wrapper = document.getElementById('wrapper');
+var wrapper_top = 12;
+var wrapper_bottom = 24;
 
+wrapper.style.minHeight = (inner_height-(wrapper_top+wrapper_bottom)) + "px";
 
 
 
