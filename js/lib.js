52147263f353ce48e14c9fe00746742fa31bb284
kevinvanoevelen <kevin.van.oevelen@gmail.com>
2016-01-03 10:42:30 +0100

    christmas tree segments first draft

--- js/lib.js
+++ js/lib.js
@@ -1,14 +1,7 @@
 
+
 function d(mess) {
 	document.write(mess);
-};
-
-function byId(ids) {
-	document.getElementById(ids);
-};
-
-function byCl(class_name) {
-	document.getElementsByClassName(class_name);
 };
 
 function a(mess) {
